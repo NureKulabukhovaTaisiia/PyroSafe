8f7aa332d6b2609e6d0703f4611dbad97aee292d
Raccoon Reed <FourteaTeo@gmail.com>
2025-11-24 23:52:07 +0200

    fix12

--- PyroSafe/Pages/Account/Logout.cshtml.cs
+++ PyroSafe/Pages/Account/Logout.cshtml.cs
@@ -5,14 +5,12 @@ namespace PyroSafe.Pages.Account
 {
     public class LogoutModel : PageModel
     {
-        [ValidateAntiForgeryToken] // захищає від CSRF
+        [ValidateAntiForgeryToken]
         public IActionResult OnPost()
         {
-            // Очистити сесію
             HttpContext.Session.Clear();
-
-            // Переадресувати на сторінку реєстрації або логіну
             return RedirectToPage("/Account/Register");
         }
+
     }
 }
