47d87bbc6c3baff68170d6a340f34df1c511610b
Raccoon Reed <FourteaTeo@gmail.com>
2025-11-25 15:10:27 +0200

    Addeventandscenario

---
+++ PyroSafe/Pages/User/AddEventAndScenario.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PyroSafe.Pages.User
+{
+    public class AddEventAndScenarioModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
